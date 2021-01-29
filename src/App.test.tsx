3cd0696e726c8e71b9183ea433ae55a014685342
Niko Mendo <nicolasmendonca@gmail.com>
2021-01-29 12:31:40 -0300

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,20 @@
+import App from "./App";
+
+it("has John Doe as default value", () => {
+  // render the component
+  // take input value
+  // assert equals "John Doe"
+});
+
+it("changes the input value", () => {
+  // render the component
+  // query the input element
+  // trigger value change
+  // assert that the input value matches new value
+});
+
+it("submits the form", () => {
+  // same as before
+  // trigger form submit (either with Enter key or 'Save' button)
+  // assert that the form is submitted
+});
---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.scss";
+
+interface IAppProps {
+  onSubmit: (e: React.FormEvent) => void;
+}
+
+const App: React.FC<IAppProps> = ({ onSubmit }) => {
+  const [name, setName] = React.useState("John Doe");
+
+  return (
+    <form className="App__input-container" onSubmit={onSubmit}>
+      <label htmlFor="input">First Name</label>
+      <input
+        id="input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        name="firstName"
+      />
+      <button type="submit">Save</button>
+    </form>
+  );
+};
+
+export default App;
